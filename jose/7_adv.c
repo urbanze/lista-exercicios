f3d4b1d30fd670eccefbbd3ce55dcc5952406bb0
José Morais <jvitor-2005@hotmail.com>
2020-08-05 20:17:52 -0300

    Create 7_adv.c

---
+++ jose/7_adv.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+
+char table[26][4] = {
+    {'A', 'A', 'A'}, {'A', 'A', 'C'}, {'A', 'A', 'G'}, {'A', 'A', 'T'}, //ABCD
+    {'A', 'C', 'A'}, {'A', 'C', 'C'}, {'A', 'C', 'G'}, {'A', 'C', 'T'}, //EFGH
+    {'A', 'G', 'A'}, {'A', 'G', 'C'}, {'A', 'G', 'G'}, {'A', 'G', 'T'}, //IJKL
+    {'A', 'T', 'A'}, {'A', 'T', 'C'}, {'A', 'T', 'G'}, {'A', 'T', 'T'}, //MNOP
+    {'C', 'A', 'A'}, {'C', 'A', 'C'}, {'C', 'A', 'G'}, {'C', 'A', 'T'}, //QRST
+    {'C', 'C', 'A'}, {'C', 'C', 'C'}, {'C', 'C', 'G'}, {'C', 'C', 'T'}, //UVWX
+    {'C', 'G', 'A'}, {'C', 'G', 'C'} //YZ
+};
+
+void decode(char *str)
+{
+    int size = strlen(str);
+    if (size % 3)
+    {
+        printf("Error 1\n");
+        return;
+    }
+    
+    size /= 3;
+    for (int j = 0; j < size; j++)
+    {
+        char comb[3] = {str[0+(j*3)], str[1+(j*3)], str[2+(j*3)]};
+        
+        for (int i = 0; i < 26; i++)
+        {
+            if (strstr(comb, &table[i][0]) != NULL)
+            {
+                printf("%c", 'A'+i);
+            }
+        }
+    }
+    
+    printf("\n\n");
+}
+
+int main()
+{
+
+    while (1)
+    {
+        char str[200] = {0};
+        printf("Digite uma base: ");
+        scanf("%s", str);
+        
+        decode(str);
+    }
+    
+    
+    
+
+    return 0;
+}
